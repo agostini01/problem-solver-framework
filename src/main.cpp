7c255df271bfd62d9ae07627075db4cbfdd11cb6
Nicolas Agostini <n.b.agostini@gmail.com>
2017-02-04 15:56:48 -0500

    Add commandline support to base project

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,129 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "util.h"
-#include <exception>
 #include <iostream>
+#include <sys/time.h>
+
+#include "../lib/cpp/Terminal.h"
+#include "../lib/cpp/String.h"
+#include "../lib/cpp/Error.h"
+#include "../lib/cpp/Misc.h"
+#include "../lib/cpp/CommandLine.h"
+
+#define VERSION "0.1.0v"
+
+////////////////////////////////////////////////////////////////////////////////
+// Configuration options
+////////////////////////////////////////////////////////////////////////////////
+
+//bool binary_hex_dump=0;
+
+////////////////////////////////////////////////////////////////////////////////
+// Variable declarations
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Function declarations
+////////////////////////////////////////////////////////////////////////////////
+
+/// Print welcome message
+void WelcomeMessage(std::ostream &os) {   
+    // Compute simulation ID
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    unsigned min_id = misc::StringAlnumToInt("10000");
+    unsigned max_id = misc::StringAlnumToInt("ZZZZZ");
+    unsigned id = (tv.tv_sec * 1000000000 + tv.tv_usec)
+            % (max_id - min_id + 1)
+            + min_id;
+    std::string alnum_id = misc::StringIntToAlnum(id);
+
+    // Blue color
+    misc::Terminal::Blue(os);
+
+    // Print welcome message in the standard error output
+    os << '\n' << "; Problem Solver " << VERSION << " \n";
+    os << "; Please use command 'main --help' for a list of command-line options.\n";
+    os << "; Simulation alpha-numeric ID: " << alnum_id << '\n';
+    os << '\n';
+    // Reset terminal color
+    misc::Terminal::Reset(os);
+}
+
+/// Read command line
+void RegisterOptions(){
+    // Parse the command line and set flags or file output names
+
+    // Set error message
+    misc::CommandLine *command_line = misc::CommandLine::getInstance();
+    command_line->setErrorMessage("\nPlease type 'main --help' for a list of "
+                                  "valid main command-line options.");
+
+    // Set help message
+    command_line->setHelp("Syntax:"
+                          "\n\n"
+                          "$ main [<options>]"
+                          "\n\n"
+                          "main's command line can take arguments "
+                          "The following list of command-line options can be used "
+                          "for <options>:");
+
+    // Help message for memory system
+    //    command_line->RegisterBool("--binary-hex-dump",
+    //                               binary_hex_dump,
+    //                               "Print the binary file in hex format"
+    //                               );
+}
+
+/// Process command line
+void ProcessOptions(){
+
+}
+
+/// Main simulation loop
+int MainLoop(){
+
+}
+
+/// Dump statistics
+void DumpReports(){
+
+}
+
+/// Main loop
+int MainProgram(int argc, char **argv){
+
+    // Print welcome message in standard error output
+    WelcomeMessage(std::cerr);
+
+    // Read command line
+    RegisterOptions();
+
+    // Process command line
+    ProcessOptions();
+
+    // Main simulation loop
+    MainLoop();
+
+    DumpReports();
+
+    // Success
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Main loop
+////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char **argv) {
-  // Main exception handler
-
-  try {
-    // Run main program
-    std::cout << "Result of InitialTest(): " << InitialTest() << std::endl;
-    return 0;
-  } catch (std::exception &e) {
-    std::cerr << e.what();
-    return 1;
-  }
+    // Main exception handler
+    try {
+        // Run main program
+        return MainProgram(argc, argv);
+    } catch (std::exception &e) {
+        std::cerr << e.what();
+        return 1;
+    }
 }
