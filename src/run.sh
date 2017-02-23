54e30a32e527cb36be22b9cb2e2a6a497de51f5f
Matan Kaminski <matan.kaminski@gmail.com>
2017-02-23 11:23:08 -0500

    Updating some problems to have a non-optimal first fit solution

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-LISTOFFILES=list_of_5_items.txt
+#LISTOFFILES=list_of_5_items.txt
+LISTOFFILES=../res/list_of_files.txt
 LOGDIR=logs/
 ./main --from-files ${LISTOFFILES} --max-solving-time 60 --exhaustive > ${LOGDIR}one_min.log
 ./main --from-files ${LISTOFFILES} --max-solving-time 600 --exhaustive > ${LOGDIR}ten_min.log
