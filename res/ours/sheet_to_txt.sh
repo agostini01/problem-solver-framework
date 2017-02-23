2988bcae050bbbfab8b30fa66c4026bfe7b5e9a6
Matan Kaminski <matan.kaminski@gmail.com>
2017-02-23 01:47:09 -0500

    Adding more samples

--- res/ours/sheet_to_txt.sh
+++ res/ours/sheet_to_txt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in `seq 1 20`; do
-   title=`cat raw.txt | awk "{print $"${i}"}" | grep _`
-   cat raw.txt | awk "{print $"${i}"}" | grep -v _ > "${title}.txt"
+   title=`cat raw.txt | awk "{print $"${i}"}" | grep HARD`
+   cat raw.txt | awk "{print $"${i}"}" | grep -v HARD > "${title}.txt"
    sed -i '/^$/d' "${title}.txt"
 done
